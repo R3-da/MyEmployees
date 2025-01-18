05f67d33d63d74efe2943c5623e2d7fddbd21ab3
r3-da <haddan.reda.net@gmail.com>
2025-01-18 22:25:38 +0100

    add logging for Swagger UI URL on application start

--- src/main/java/com/hahn/myemployees/config/OpenAPIConfig.java
+++ src/main/java/com/hahn/myemployees/config/OpenAPIConfig.java
@@ -1,12 +1,31 @@
 package com.hahn.myemployees.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class OpenAPIConfig {
+    
+    private static final Logger logger = LoggerFactory.getLogger(OpenAPIConfig.class);
+    
+    private final Environment environment;
+
+    public OpenAPIConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void logSwaggerUiUrl() {
+        String port = environment.getProperty("server.port", "8080");
+        logger.info("Swagger UI is available at: http://localhost:{}/swagger-ui.html", port);
+    }
     
     @Bean
     public OpenAPI myEmployeesOpenAPI() {
